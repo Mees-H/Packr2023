c715eb1f4660d21e2a0d4cc3bbe78b68f334578e
paulbergmans <pta.bergmans@student.avans.nl>
2023-04-01 21:03:06 +0200

    weather

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ form.resetForm();
 let conveyorbelts = new Conveyorbelt(docks);
 conveyorbelts.addConveyorbelt(1);
 conveyorbelts.addConveyorbelt(2);
+
+let weather = new Weather("s'Hertogenbosch");
---
+++ weather.js
@@ -0,0 +1,77 @@
+class Weather {
+    location;
+
+    constructor(location) {
+        this.location = location;
+        this.getWeatherFromApi();
+        this.createChangeWeatherForm();
+    }
+    
+    createChangeWeatherForm() {
+        let formgroup = document.createElement("div");
+        formgroup.className = "form-group small-form-group ml-2 mt-2 mb-2 border border-dark rounded";
+        formgroup.id = "size";
+
+        let label = document.createElement("label");
+        label.textContent = "Change Location:";
+
+        let input = document.createElement("input");
+        input.setAttribute("type", "newWeather");
+        input.className = "form-control";
+        input.id = "newWeather";
+        input.setAttribute("placeholder", "New Location")
+
+        let button = document.createElement("button");
+        button.className = "btn btn-primary mt-2 ml";
+        button.textContent = "Change location";
+        button.id = "changeLocation";
+
+        button.onclick = () => {
+            this.changeWeatherLocation(input.value);
+        };
+
+        formgroup.appendChild(label);
+        formgroup.appendChild(input);
+        formgroup.appendChild(button);
+
+        document.getElementById("weather-form").appendChild(formgroup);
+    }
+
+    changeWeatherLocation(location) {
+        let form = document.getElementById("weather");
+        form.innerHTML = "";
+        this.location = location;
+        this.getWeatherFromApi();
+    }
+
+    async getWeatherFromApi() {
+        let response = await fetch("http://api.weatherapi.com/v1/current.json?key=8a52679ad6f243a98e2144608230104&q="+this.location+"&aqi=no")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                this.setCurrentWeather(data.location.name, data.current.temp_c, data.current.condition.text, data.current.wind_kph);
+            });
+    }
+
+    setCurrentWeather(location, temperature, conditionText, windspeed) {
+        let form = document.getElementById("weather");
+
+        let formgroup = document.createElement("div");
+        formgroup.className = "form-group small-form-group ml-2 mt-2 mb-2 border border-dark rounded";
+        formgroup.id = "size";
+
+        let title = document.createElement("h5");
+        title.textContent = "Current Weather:";
+        title.className = "centeralign"
+        title.id = "weatherTitle";
+        formgroup.appendChild(title);
+
+        let button = document.createElement("h6");
+        button.textContent = "The weather condition in "+location+" is "+conditionText+" with a temperature of "+temperature+"C and a windspeed of "+windspeed+"KPH.";
+        button.className = "centeralign"
+        button.id = "weatherNumber";
+        formgroup.appendChild(button);
+
+        form.appendChild(formgroup);
+    }
+}
