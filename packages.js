d32dd0579a8f44fd5e8446d5a27d25a06ee1eb50
Mees-H <meesconfirmed@gmail.com>
2023-04-01 16:17:57 +0200

    added placement of packages

--- packages.js
+++ packages.js
@@ -69,7 +69,6 @@ class Package {
             let shiftX = event.clientX - tetriomino.getBoundingClientRect().left;
             let shiftY = event.clientY - tetriomino.getBoundingClientRect().top;
 
-            
             function moveAt(pageX,pageY) {
                 tetriomino.style.left = pageX - shiftX - 250 + "px";
                 tetriomino.style.top = pageY - shiftY + "px";
@@ -84,8 +83,8 @@ class Package {
                 let timeout = undefined;
                 if(event.target.closest('.package') === null) {
                     timeout = setTimeout(function() {
-                        document.removeEventListener("mousemove", onMouseMove)
-                    }, 2000)
+                        tetriomino.reset();
+                    }, 3000)
                 }
                 else {
                     clearTimeout(timeout);
@@ -116,11 +115,51 @@ class Package {
             document.addEventListener("mousemove", onMouseMove);
         
             tetriomino.onmouseup = function() {
+
+                if(currentDroppable != null) {
+                    
+                    let Xvar = 0;
+                    let Yvar = 0;
+
+                    if (shiftX > 25 && shiftX <= 50) {
+                        Xvar = -25;
+                    }
+
+                    else if (shiftX > 50 && shiftX <= 75) {
+                        Xvar = -50;
+                    }
+
+                    if (shiftY > 25 && shiftY <= 50) {
+                        Yvar = -25;
+                    }
+
+                    else if (shiftY > 50 && shiftY <= 75) {
+                        Yvar = -50;
+                    }
+
+                    tetriomino.style.left = Xvar + "px";
+                    tetriomino.style.top = Yvar + "px";
+
+                    checkOverlap(tetriomino);
+
+                    currentDroppable.appendChild(tetriomino);
+
+                    tetriomino.style.position = "relative";
+
+                    tetriomino.classList.add("avoid-clicks");
+                }
+
+                document.removeEventListener("mousemove", onMouseMove);
+                tetriomino.onmouseup = null;
+                
+            }
+
+            tetriomino.reset = function() {
                 let left = Math.round(tetriomino.style.left.slice(0, -2)/25)*25;
                 tetriomino.style.left = left + "px";
                 let top = Math.round(tetriomino.style.top.slice(0, -2)/25)*25;
                 tetriomino.style.top = top + "px";
-
+                
                 document.removeEventListener("mousemove", onMouseMove);
                 tetriomino.onmouseup = null;
             }
@@ -129,8 +168,15 @@ class Package {
                 elem.style.background = 'pink';
             }
           
-              function leaveDroppable(elem) {
+            function leaveDroppable(elem) {
                 elem.style.background = '';
+            }
+
+            function checkOverlap(tetriomino) {
+                let grids = document.getElementsByClassName('griditem');
+                Array.from(grids).forEach(element => {
+                    
+                });
             }
         
             tetriomino.ondragstart = function() {
